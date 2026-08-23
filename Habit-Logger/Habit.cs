1c2306a980c89e3a8ac582e8f78c894f53373b54
Tetris <szymon.nowakowski2807@gmail.com>
2026-08-23 17:58:14 +0200

    Create Habit class

---
+++ Habit-Logger/Habit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habit_Logger
+{
+    internal class Habit
+    {
+        public int Id { get; init; }
+        public required string HabitName { get; set; }
+        public DateOnly Date { get; set; }
+        public int Quantity { get; set; }
+    }
+}
